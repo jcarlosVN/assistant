2cf0f78dd96c8c30f00e8d9ff03bcfcee8977328
jcarlosVN <106927124+jcarlosVN@users.noreply.github.com>
2024-07-16 00:36:45 -0500

    Add files via upload

--- test_assistant_functionCalling.py
+++ test_assistant_functionCalling.py
@@ -83,47 +83,50 @@ def run_assistant(thread, assistant):
     new_message = messages.data[0].content[0].text.value
     return new_message, messages, run
 
-
 new_message, messages, run, thread, assistant = run_conversation()
 
 tool_outputs = []
- 
+
 # Loop through each tool in the required action section
-for tool in run.required_action.submit_tool_outputs.tool_calls:
-  if tool.function.name == "get_current_temperature":
-    tool_outputs.append({
-      "tool_call_id": tool.id,
-      "output": "57"
-    })
-  elif tool.function.name == "get_rain_probability":
-    tool_outputs.append({
-      "tool_call_id": tool.id,
-      "output": "0.06"
-    })
- 
+for tool_call in run.required_action.submit_tool_outputs.tool_calls:
+    if tool_call.function.name == "get_current_temperature":
+        arguments = json.loads(tool_call.function.arguments)
+        location = arguments['location']
+        print(arguments, location)
+        tool_outputs.append({
+            "tool_call_id": tool_call.id,
+            "output": get_current_temperature(location)
+        })
+    elif tool_call.function.name == "get_rain_probability":
+        tool_outputs.append({
+            "tool_call_id": tool_call.id,
+            "output": "0.06"
+        })
+
 # Submit all tool outputs at once after collecting them in a list
 if tool_outputs:
-  try:
-    run = client.beta.threads.runs.submit_tool_outputs_and_poll(
-      thread_id=thread.id,
-      run_id=run.id,
-      tool_outputs=tool_outputs
-    )
-    print("Tool outputs submitted successfully.")
-    print("-----------------------")
-  except Exception as e:
-    print("Failed to submit tool outputs:", e)
-    print("-----------------------")
+    try:
+        run = client.beta.threads.runs.submit_tool_outputs_and_poll(
+            thread_id=thread.id,
+            run_id=run.id,
+            tool_outputs=tool_outputs
+        )
+        print("Tool outputs submitted successfully.")
+        print("-----------------------")
+    except Exception as e:
+        print("Failed to submit tool outputs:", e)
+        print("-----------------------")
 else:
-  print("No tool outputs to submit.")
- 
+    print("No tool outputs to submit.")
+
 if run.status == 'completed':
-  messages = client.beta.threads.messages.list(
-    thread_id=thread.id
-  )
-  print(messages)
-  print("-----------------------")
-  #message_text = messages['data'][0]['content'][0]['text']['value']
-  #print(message_text)
+    messages = client.beta.threads.messages.list(
+        thread_id=thread.id
+    )
+    print(messages)
+    print("-----------------------")
+    if messages.data:
+        message_text = messages.data[0].content[0].text.value
+        print(message_text)
 else:
-  print(run.status)
+    print(run.status)
